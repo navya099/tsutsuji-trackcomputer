24b6de945b909b7ec2171dee9a97c3a475670e95
navya099 <130723074+navya099@users.noreply.github.com>
2025-04-10 22:17:23 +0900

    Update math.py
    위도경도 국내좌표계 반영
    EPSG 4236 TO 5186

--- tsutsuji/math.py
+++ tsutsuji/math.py
@@ -1,5 +1,5 @@
 #
-#    Copyright 2021-2024 konawasabi
+#    Copyright 2021-2022 konawasabi
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
@@ -18,6 +18,9 @@
 '''
 
 import numpy as np
+import pandas as pd
+import pyproj
+import folium
 
 def rotate(tau1):
     '''２次元回転行列を返す。
@@ -118,44 +121,6 @@ def cross_normal(position, track):
     Return:
     '''
     return None
-
-def mindist_crossline(position, phiA, track):
-    '''曲線trackとpositionを通る方位角phiAの直線Uの交点について、positionとの距離が最小となるものを求める
-
-    track上の各点をT_k, positionをP, 座標原点Oとして、直線Uに沿う単位ベクトルをeUで表す。
-    OT_k - (OP + α * eU) が最小となるようなαを求める。 
-
-    Args:
-         position (ndarray):
-           np.array([x,y])
-    
-         phiA (float):
-           絶対座標x軸に対して直線がなす角
-    
-         track (ndarray):
-           np.array([[x0,y0],[x1,y1],...,[xn,yn]])
-
-    Return:
-        np.array:
-           [[sort_ix: 交点との距離が最小となる点のインデックス, alpha: positionから点列交点の距離, distance: 求めた交点から点列の距離],...]
-        : 
-    '''
-
-    eU = np.array([np.cos(phiA),np.sin(phiA)])
-    alpha = np.dot(track - position, eU)
-    dist_v = (track - (alpha.reshape(-1,1)*eU + position))**2
-    distance = np.sqrt(dist_v[:,0] + dist_v[:,1])
-
-    sort_ix = np.argsort(distance) # 点列と交点の距離が小さい順にソートしたインデックスを得る
-
-    '''
-    result = []
-    for i in range(0,5):
-        result.append([alpha[sort_ix[i]],sort_ix[i],distance[sort_ix[i]]])
-    '''
-    
-    return np.vstack((sort_ix,alpha[sort_ix],distance[sort_ix])).T
-
 def angle_twov(phiA, phiB):
     ''' ベクトルA (方位角phiA)からベクトルB(方位角phiB)への方位角変化を求める
     '''
@@ -181,162 +146,46 @@ def interpolate_with_dist(track, element, cp_dist):
     return result
 
 def calc_pl2xy(phi_deg, lambda_deg, phi0_deg, lambda0_deg):
-    """ 緯度経度を平面直角座標に変換する
-    - input:
-        (phi_deg, lambda_deg): 変換したい緯度・経度[度]（分・秒でなく小数であることに注意）
-        (phi0_deg, lambda0_deg): 平面直角座標系原点の緯度・経度[度]（分・秒でなく小数であることに注意）
-    - output:
-        x: 変換後の平面直角座標[m]
-        y: 変換後の平面直角座標[m]
-        * x軸正方向は北、y軸正方向は東を指すことに注意
+    #phi_deg=위도 lambda_deg경도
+    """ 위도 경도를 평면 직각 좌표로 변환
+     - input:
+         (phi_deg, lambda_deg) : 변환하고 싶은 위도·경도[도](분·초가 아닌 소수인 것에 주의)
+         (phi0_deg, lambda0_deg): 평면 직각 좌표계 원점의 위도·경도[도](분·초가 아닌 소수인 것에 주의)
+     - output:
+         x: 변환 후 평면 직각 좌표[m]
+         y: 변환 후 평면 직각 좌표[m]
+         * x축 정방향은 북, y축 정방향은 동쪽을 가리키는 것에 주의
         
-        原典
-        https://qiita.com/sw1227/items/e7a590994ad7dcd0e8ab
-    """
-    # 緯度経度・平面直角座標系原点をラジアンに直す
-    phi_rad = np.deg2rad(phi_deg)
-    lambda_rad = np.deg2rad(lambda_deg)
-    phi0_rad = np.deg2rad(phi0_deg)
-    lambda0_rad = np.deg2rad(lambda0_deg)
-
-    # 補助関数
-    def A_array(n):
-        A0 = 1 + (n**2)/4. + (n**4)/64.
-        A1 = -     (3./2)*( n - (n**3)/8. - (n**5)/64. ) 
-        A2 =     (15./16)*( n**2 - (n**4)/4. )
-        A3 = -   (35./48)*( n**3 - (5./16)*(n**5) )
-        A4 =   (315./512)*( n**4 )
-        A5 = -(693./1280)*( n**5 )
-        return np.array([A0, A1, A2, A3, A4, A5])
-
-    def alpha_array(n):
-        a0 = np.nan # dummy
-        a1 = (1./2)*n - (2./3)*(n**2) + (5./16)*(n**3) + (41./180)*(n**4) - (127./288)*(n**5)
-        a2 = (13./48)*(n**2) - (3./5)*(n**3) + (557./1440)*(n**4) + (281./630)*(n**5)
-        a3 = (61./240)*(n**3) - (103./140)*(n**4) + (15061./26880)*(n**5)
-        a4 = (49561./161280)*(n**4) - (179./168)*(n**5)
-        a5 = (34729./80640)*(n**5)
-        return np.array([a0, a1, a2, a3, a4, a5])
-
-    # 定数 (a, F: 世界測地系-測地基準系1980（GRS80）楕円体)
-    m0 = 0.9999 
-    a = 6378137.
-    F = 298.257222101
-
-    # (1) n, A_i, alpha_iの計算
-    n = 1. / (2*F - 1)
-    A_array = A_array(n)
-    alpha_array = alpha_array(n)
-
-    # (2), S, Aの計算
-    A_ = ( (m0*a)/(1.+n) )*A_array[0] # [m]
-    S_ = ( (m0*a)/(1.+n) )*( A_array[0]*phi0_rad + np.dot(A_array[1:], np.sin(2*phi0_rad*np.arange(1,6))) ) # [m]
-
-    # (3) lambda_c, lambda_sの計算
-    lambda_c = np.cos(lambda_rad - lambda0_rad)
-    lambda_s = np.sin(lambda_rad - lambda0_rad)
-
-    # (4) t, t_の計算
-    t = np.sinh( np.arctanh(np.sin(phi_rad)) - ((2*np.sqrt(n)) / (1+n))*np.arctanh(((2*np.sqrt(n)) / (1+n)) * np.sin(phi_rad)) )
-    t_ = np.sqrt(1 + t*t)
-
-    # (5) xi', eta'の計算
-    xi2  = np.arctan(t / lambda_c) # [rad]
-    eta2 = np.arctanh(lambda_s / t_)
-
-    # (6) x, yの計算
-    x = A_ * (xi2 + np.sum(np.multiply(alpha_array[1:],
-                                       np.multiply(np.sin(2*xi2*np.arange(1,6)),
-                                                   np.cosh(2*eta2*np.arange(1,6)))))) - S_ # [m]
-    y = A_ * (eta2 + np.sum(np.multiply(alpha_array[1:],
-                                        np.multiply(np.cos(2*xi2*np.arange(1,6)),
-                                                    np.sinh(2*eta2*np.arange(1,6)))))) # [m]
-    # return
-    return x, y # [m]
-
+         원전
+         https://qiita.com/sw1227/items/e7a590994ad7dcd0e8ab
+     """
+    
+    p1_type = pyproj.CRS.from_epsg(4326)
+    p2_type = pyproj.CRS.from_epsg(5186)
+    transformer = pyproj.Transformer.from_crs(p1_type, p2_type, always_xy=True)
+    y,x = transformer.transform(lambda_deg,phi_deg)
+    #x위도、y경도
+    return x, y  # [m]
+    
 def calc_xy2pl(x, y, phi0_deg, lambda0_deg):
-    """ 平面直角座標を緯度経度に変換する
-    - input:
-        (x, y): 変換したいx, y座標[m]
-        (phi0_deg, lambda0_deg): 平面直角座標系原点の緯度・経度[度]（分・秒でなく小数であることに注意）
-        * x軸正方向は北、y軸正方向は東を指すことに注意
-    - output:
-        latitude:  緯度[度]
-        longitude: 経度[度]
-        * 小数点以下は分・秒ではないことに注意
+    """평면 직각 좌표를 위도 경도로 변환
+     - input:
+         (x, y): 변환하려는 x, y 좌표[m]
+         (phi0_deg, lambda0_deg): 평면 직각 좌표계 원점의 위도·경도[도](분·초가 아닌 소수인 것에 주의)
+         * x축 정방향은 북, y축 정방향은 동쪽을 가리키는 것에 주의
+     - output:
+         latitude: 위도[도]
+         longitude: 경도[도]
+         * 소수점 이하는 분·초가 아닌 것에 주의
         
-        原典
-        https://qiita.com/sw1227/items/e7a590994ad7dcd0e8ab
-    """
-    # 平面直角座標系原点をラジアンに直す
-    phi0_rad = np.deg2rad(phi0_deg)
-    lambda0_rad = np.deg2rad(lambda0_deg)
-
-    # 補助関数
-    def A_array(n):
-        A0 = 1 + (n**2)/4. + (n**4)/64.
-        A1 = -     (3./2)*( n - (n**3)/8. - (n**5)/64. ) 
-        A2 =     (15./16)*( n**2 - (n**4)/4. )
-        A3 = -   (35./48)*( n**3 - (5./16)*(n**5) )
-        A4 =   (315./512)*( n**4 )
-        A5 = -(693./1280)*( n**5 )
-        return np.array([A0, A1, A2, A3, A4, A5])
-
-    def beta_array(n):
-        b0 = np.nan # dummy
-        b1 = (1./2)*n - (2./3)*(n**2) + (37./96)*(n**3) - (1./360)*(n**4) - (81./512)*(n**5)
-        b2 = (1./48)*(n**2) + (1./15)*(n**3) - (437./1440)*(n**4) + (46./105)*(n**5)
-        b3 = (17./480)*(n**3) - (37./840)*(n**4) - (209./4480)*(n**5)
-        b4 = (4397./161280)*(n**4) - (11./504)*(n**5)
-        b5 = (4583./161280)*(n**5)
-        return np.array([b0, b1, b2, b3, b4, b5])
-
-    def delta_array(n):
-        d0 = np.nan # dummy
-        d1 = 2.*n - (2./3)*(n**2) - 2.*(n**3) + (116./45)*(n**4) + (26./45)*(n**5) - (2854./675)*(n**6)
-        d2 = (7./3)*(n**2) - (8./5)*(n**3) - (227./45)*(n**4) + (2704./315)*(n**5) + (2323./945)*(n**6)
-        d3 = (56./15)*(n**3) - (136./35)*(n**4) - (1262./105)*(n**5) + (73814./2835)*(n**6)
-        d4 = (4279./630)*(n**4) - (332./35)*(n**5) - (399572./14175)*(n**6)
-        d5 = (4174./315)*(n**5) - (144838./6237)*(n**6)
-        d6 = (601676./22275)*(n**6)
-        return np.array([d0, d1, d2, d3, d4, d5, d6])
-
-    # 定数 (a, F: 世界測地系-測地基準系1980（GRS80）楕円体)
-    m0 = 0.9999 
-    a = 6378137.
-    F = 298.257222101
-
-    # (1) n, A_i, beta_i, delta_iの計算
-    n = 1. / (2*F - 1)
-    A_array = A_array(n)
-    beta_array = beta_array(n)
-    delta_array = delta_array(n)
-
-    # (2), S, Aの計算
-    A_ = ( (m0*a)/(1.+n) )*A_array[0]
-    S_ = ( (m0*a)/(1.+n) )*( A_array[0]*phi0_rad + np.dot(A_array[1:], np.sin(2*phi0_rad*np.arange(1,6))) )
-
-    # (3) xi, etaの計算
-    xi = (x + S_) / A_
-    eta = y / A_
-
-    # (4) xi', eta'の計算
-    xi2 = xi - np.sum(np.multiply(beta_array[1:], 
-                                  np.multiply(np.sin(2*xi*np.arange(1,6)),
-                                              np.cosh(2*eta*np.arange(1,6)))))
-    eta2 = eta - np.sum(np.multiply(beta_array[1:],
-                                   np.multiply(np.cos(2*xi*np.arange(1,6)),
-                                               np.sinh(2*eta*np.arange(1,6)))))
-
-    # (5) chiの計算
-    chi = np.arcsin( np.sin(xi2)/np.cosh(eta2) ) # [rad]
-    latitude = chi + np.dot(delta_array[1:], np.sin(2*chi*np.arange(1, 7))) # [rad]
-
-    # (6) 緯度(latitude), 経度(longitude)の計算
-    longitude = lambda0_rad + np.arctan( np.sinh(eta2)/np.cos(xi2) ) # [rad]
-
-    # ラジアンを度になおしてreturn
-    return np.rad2deg(latitude), np.rad2deg(longitude) # [deg]
+         원전
+         https://qiita.com/sw1227/items/e7a590994ad7dcd0e8ab
+     """
+    p1_type = pyproj.CRS.from_epsg(5186)
+    p2_type = pyproj.CRS.from_epsg(4326)
+    transformer = pyproj.Transformer.from_crs(p1_type, p2_type, always_xy=False)
+    latitude, longitude = transformer.transform(x, y)
+    return latitude, longitude # [deg]
 
 def long2px(l, z):
     '''経度をマップタイルのピクセル座標に変換する
@@ -379,28 +228,3 @@ def py2lat(y, z, L=85.05112878):
         l: 緯度 [deg]
     '''
     return 180/np.pi*(np.arcsin(np.tanh(-np.pi/(2**(z+7))*y+np.arctanh(np.sin(np.pi/180*L)))))
-
-def crosspoint_2lines(A, eA, B, eB):
-    '''2直線X, Yの交点Pを求める
-
-    Args:
-         A  (ndarray): 直線Xの通過点
-         eA (ndarray): 直線Xの単位ベクトル
-         B  (ndarray): 直線Yの通過点
-         eB (ndarray): 直線Yの単位ベクトル
-
-    Return:
-        (ndarray, ndarray): (PAの距離, 点Pの座標)
-    '''
-    alpha = (np.dot(eA,eB)*np.dot(A-B,eB)-np.dot(A-B,eA))/(1-(np.dot(eA,eB))**2)
-    return (alpha,A+alpha*eA)
-
-def phi2el(phi):
-    '''角度phi [rad]の単位ベクトルを返す
-    Args:
-        phi (float): 求める単位ベクトルが座標軸に対してなす角 [rad]
-    Return:
-        np.array([float, float])
-    '''
-
-    return np.array([np.cos(phi),np.sin(phi)])
